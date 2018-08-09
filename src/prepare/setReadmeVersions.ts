6de0ace6cc5a0ff62247a0cb663ad6eb3642b276
Arturas <a.molcanovas@gmail.com>
2018-08-09 23:37:42 +0100

    fix(setReadmeVersion): No longer replace branch name with "my-branch"

--- src/prepare/setReadmeVersions.ts
+++ src/prepare/setReadmeVersions.ts
@@ -6,6 +6,7 @@ import {Context} from '../Context';
 export async function setReadmeVersions(_cfg: Conf, ctx: Context): Promise<void> {
   const readmePath = join(ctx.cwd, 'README.md');
   const {log} = ctx.logger;
+  const v = ctx.nextRelease.version;
 
   let readmeContents: string;
   try {
@@ -19,15 +20,15 @@ export async function setReadmeVersions(_cfg: Conf, ctx: Context): Promise<void>
   const replacements: any[][] = [
     [
       /\?branch=([a-z0-9.\-_]+)/ig,
-      `?branch=${ctx.nextRelease.version}`
+      `?branch=${v}`
     ],
     [
       /img\.shields\.io\/travis(\/com)?\/([a-z0-9-_.]+)\/([a-z0-9-_.]+)\/([a-z0-9-_.]+)\.([a-z]{2,4})/ig,
-      'img.shields.io/travis$1/$2/$3/my-branch.$5'
+      `img.shields.io/travis$1/$2/$3/${v}.$5`
     ],
     [
       /img\.shields\.io\/coveralls\/github\/([a-z0-9-_.]+)\/([a-z0-9-_.]+)\/([a-z0-9-_.]+)\.([a-z]{2,4})/ig,
-      'img.shields.io/coveralls/github/$1/$2/my-branch.$4'
+      `img.shields.io/coveralls/github/$1/$2/${v}.$4`
     ]
   ];
 
