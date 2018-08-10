1951c96d99341c4fcf5c71d3dba382f71db3fffa
Arturas <a.molcanovas@gmail.com>
2018-08-10 09:47:22 +0100

    fix(prepare): Remove debug log entry

--- src/prepare/index.ts
+++ src/prepare/index.ts
@@ -3,6 +3,5 @@ import {Context} from '../Context';
 import {setReadmeVersions} from './setReadmeVersions';
 
 export async function prepare(_cfg: Conf = {}, ctx: Context) {
-  ctx.logger.log('foo');
   await setReadmeVersions(_cfg, ctx);
 }
